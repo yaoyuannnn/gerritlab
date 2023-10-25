4b737e4eed46a6c773696c2ef6fd6b1a6a4348cd
Ahmon Dancy <adancy@wikimedia.org>
2023-10-25 07:46:23 -0700

    setup.py: Bump to version 2.0.0
    #54 changed how Gerritlab links local commits with merge requests, so
    bumping the version number to reflect this incompatible change.
    
    Change-Id: I016dd46ca3eb5ec702149ee1991ba10943572490

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="gerritlab",
-    version="1.0.0",
+    version="2.0.0",
     description="Gerrit-like code review for GitLab",
     author="Yuan Yao",
     author_email="yaoyuannnn@gmail.com",
